219a18c2726718052c8d4b9e2108b6e34d19342f
fonea10 <59515619+fonea10@users.noreply.github.com>
2020-04-02 20:24:44 +0300

    Update bug_db.sql

--- bug_db.sql
+++ bug_db.sql
@@ -6,7 +6,7 @@
 -- Generation Time: Apr 02, 2020 at 03:48 PM
 -- Server version: 10.1.16-MariaDB
 -- PHP Version: 7.0.9
-
+use bug_db;
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
